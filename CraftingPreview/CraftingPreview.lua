4069df3ecd3fa6098f810cd8738f3abebae5d344
Shinni <shinni@binaryvector.net>
2018-03-03 22:33:04 +0100

    movable tooltip and (buggy) gamepad support

--- CraftingPreview/CraftingPreview.lua
+++ CraftingPreview/CraftingPreview.lua
@@ -1,16 +1,33 @@
 
 local PREVIEW = LibStub("LibPreview")
 
+local function MoveTooltip(self)
+	self.creationPanel.resultTooltip:ClearAnchors()
+	self.creationPanel.resultTooltip:SetAnchor(RIGHT, GuiRoot, CENTER, -self.creationPanel.resultTooltip:GetWidth() / 2, -self.creationPanel.resultTooltip:GetHeight() / 2)
+	self.creationPanel.resultTooltip:SetMouseEnabled(true)
+	self.creationPanel.resultTooltip:SetMovable(true)
+end
+
+local function ResetTooltip(self)
+	self.creationPanel.resultTooltip:ClearAnchors()
+	self.creationPanel.resultTooltip:SetAnchor(BOTTOM, GuiRoot, BOTTOM, 0, -245)
+end
+
 ZO_PreHook(ZO_Smithing, "SetMode", function(self, mode)
-	if SMITHING_SCENE.state ~= SCENE_SHOWN then return end
-	-- we exit creation mode, so exit creation preview
 	if mode ~= SMITHING_MODE_CREATION then
+		ResetTooltip(self)
+		if SMITHING_SCENE.state ~= SCENE_SHOWN then return end
+		-- we exit creation mode, so exit creation preview
 		PREVIEW:DisablePreviewMode()
+	else
+		MoveTooltip(self)
 	end
 end)
 
-ZO_PreHook(ZO_SmithingCreation, "SetupResultTooltip", function(self, patternIndex, materialIndex,materialQuantity, itemStyleId, traitIndex)
+local function TooltipHook(self, patternIndex, materialIndex,materialQuantity, itemStyleId, traitIndex)
 	-- the crafting result changed, change the preview
 	local itemLink = GetSmithingPatternResultLink(patternIndex, materialIndex,materialQuantity, itemStyleId, traitIndex)
 	PREVIEW:PreviewItemLink(itemLink)
-end)
+end
+ZO_PreHook(ZO_SmithingCreation, "SetupResultTooltip", TooltipHook)
+ZO_PreHook(ZO_GamepadSmithingCreation, "SetupResultTooltip", TooltipHook)
